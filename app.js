e1123b1795b456b99c85564f62d3116e038e1afd
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2022-12-15 21:53:15 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');// para accder a las paginas
+
+app.use(express.static('public'));// nose para que es
+
+app.listen(3030,()=> console.log('Servidor corriendo en http://localhost:3030'));
+
+//Creación de rutas
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')));
+app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, './views/registro.html')));
