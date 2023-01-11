93d7e55c19ca276d1d950e261655096f3c015883
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-11 12:02:31 -0300

    arreglo de todos los link de views

--- src/app.js
+++ src/app.js
@@ -1,36 +1,37 @@
 
 const express = require('express');
-const morgan = require('morgan');//linea 9
+const path = require('path');
+const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
+
 const app = express();
+
+//muestra infomacion adicional en la consela si se esta enviando informacion 
+const morgan = require('morgan');
+app.use(morgan('dev'));//muestra infomacion adicional en la consela si se esta enviando informacion 
+
+// Middlewares
+app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false })); // Para capturar el body
+app.use(express.json()); // Para capturar el body
+app.use(methodOverride('_method'));//Para crar, eliminar y modificar.. se puede poner cualquier nombre en '_method'
+
+//es para que la carpeta del proyecto public sea publicom, van html ejs css
+app.set('view engine', 'ejs'); //para usar ejs
+app.set('views', path.join(__dirname, '/views')); //Es necesario para que la carpeta views pueda estar adentro de la carpeta src// Define la ubicación de la carpeta de las Vistas
 
 // Importamos routers//const path = require('path');// para accder a las paginas
 const homeRouter = require('./routes/homeRouter.js')
 const userRouter = require('./routes/userRouter.js')
 const productRouter = require('./routes/productRouter.js')
 
-//muestra infomacion adicional en la consela si se esta enviando informacion 
-app.use(morgan('dev'));
-
-//es para que la carpeta del proyecto public sea publicom, van html ejs css
-app.use(express.static('public'));
-
-//para usar ejs
-app.set('view engine', 'ejs'); 
-
 // Usando los enrutadores importados linea 5
 app.use("/", homeRouter);
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 
-//Creación de rutas - resion anterior
-/* app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html')));
-app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, './views/registro.html')));
-
- */
-
-
-
 //Es para escuchar el servido  hacerlo que ande  en Tander
 const port = process.env.PORT || 3000;
 app.listen(port,()=> console.log('Servidor corriendo en http://localhost:3000'));
+
+
+app.use(express.static('public'));//PAra poder poner carpeta en src
