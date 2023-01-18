6f62c371f10e5a86e6d8253827853a1048bc5296
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-18 17:22:59 -0300

    validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,23 @@
+const {validationResult} = require('express-validator'); //validationResult es una funcion que tambien nos lo da express-validator 
+
 const controlador ={ //IMPORTANTE
     register:(req, res)=>{
         return res.render('register');
     },
+    processRegister:(req,res)=>{
+        const resultValidation = validationResult(req);
+
+        if (resultValidation.errors.length > 0){//resultValidation.errors es un objeto literal
+        return res.render('register', {errors: resultValidation.mapped()}) //mapped: pasa la variable resultValidation a literiario
+            errors: resultValidation.mapped();
+        }
+    },
     loguearse:(req, res)=>{
         return res.render('login');
     },
+    processLoguearse:(req, res) => {
+        return res.send("esta en el proceso de login")
+    }
 }
 
         //exportamos el objeto literal con sus metodos
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,9 +3,21 @@
 //llama expres y guarda la ejecucion de router
 const express = require("express");
 const router = express.Router();
+const {body} = require("express-validator");//para validar solo lo que nos pasa el body//Tambien en vez de body podemos usar check()
 
 // llamamos a la ruta de controlador
-const userController = require("../controllers/userController.js")
+const userController = require("../controllers/userController.js");
+
+//ponemos en una variable las validaciones que necesitamos
+//https://github.com/validatorjs/validator.js Para buscar
+const validations =[
+    body('firstName').notEmpty().withMessage('Tienes que escribir tu nombre'),  //firstName sale del name de ejs. notEmty es una validacion, valida si el campo esta vacio
+    body('lastName').notEmpty().withMessage('Tienes que escribir tu apellido'),//withMessage: para cambiar el mesaje de error
+    body('email').notEmpty().withMessage('Tienes que escribir tu email'),
+    body('password').notEmpty().withMessage('Tienes que escribir un password'),
+    body('repeatPassword').notEmpty().withMessage('Tienes que escribir un password'),
+];
+
 
 // procesa pedido de get. Ahora usamos router en MVC. son tutas 
 /* router.get("/list", mainController.list); */ //EJEMPLO DE PRAMETRO COMPARTIDO
@@ -13,8 +25,10 @@ const userController = require("../controllers/userController.js")
 // procesa pedido de get. Ahora usamos router en MVC. son tutas 
 //para ir a la pagina de registro
 router.get("/register/", userController.register);
+router.post("/register/", validations, userController.processRegister);//Validarios entre la direccion el controlador
 //para ir a la pagina de login
 router.get("/login/", userController.loguearse);
+router.post("/login/", userController.processLoguearse);
 
 
 module.exports = router;
