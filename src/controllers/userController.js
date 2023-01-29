d9d8941777774a39e7130bbf0d1446ab428103e3
Sebastian-velazquez <sergiovelazquez1693@gmail.com>
2023-01-28 23:53:53 -0300

    registro terminado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const {validationResult} = require('express-validator'); //validationResult es una funcion que tambien nos lo da express-validator 
+const bcryptjs = require('bcryptjs');//para encriptar contrasenias
+//models
 const User = require('../models/User')
 
 const controlador ={ //IMPORTANTE
@@ -12,16 +14,32 @@ const controlador ={ //IMPORTANTE
         return res.render('register', {
             errors: resultValidation.mapped(), //mapped: pasa la variable resultValidation a literiario 
             oldData: req.body //Para mostrar los datos bien ingresados
-            }) 
+            });
         }
 
+
+        //Validamos si ya existe el mail ingresado antes de cargar el usuario nuevo
+        let userInDB = User.findByField('email', req.body.email)
+        if (userInDB){
+            return res.render('register', {
+                errors: {
+                    email: {msg:'Este email ya esta registrado'}
+                }, //mapped: pasa la variable resultValidation a literiario 
+                oldData: req.body //Para mostrar los datos bien ingresados
+                }) ; 
+        }
+
+
+
+        //crear el ususario
         let userToCreate = {//no me qedo entendido .. creo que es oara sacar el pash y no mostrar toda la infomacion del la ruta
             ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10),//le doy el password del body y el dias es la encriptacion
             image: req.file.filename
         }
 
-        User.create(userToCreate);
-        return res.send('Te registrarte con exito')
+        let userCreated = User.create(userToCreate);
+        return res.redirect('/user/login')
     },
     loguearse:(req, res)=>{
         return res.render('login');
